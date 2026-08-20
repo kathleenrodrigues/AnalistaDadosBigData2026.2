161dc037890d33c7220a5d1fa30aa09255029193
KATHELEEN PEREIRA RODRIGUES <Katheleen.rodrigues@aluno.senac.rio>
2026-08-19 21:40:23 -0300

    adição de material

---
+++ aula04/aula0401.py
@@ -0,0 +1,95 @@
+# nome = input("Informe seu nome:")
+
+# if nome=="Pyetro":
+#     resultado="Pyetro presente!"
+# elif nome== "Phellipe":
+#     resultado="Phellipe presente!"
+
+
+# #-----------------------------------------------------
+# #visão if
+#mes = int(input("Informe o mês de nascimento em N°:"))
+
+# if mes==1:
+#     signo="Aquário"
+# elif mes==2:
+#     signo="Peixes"
+# elif mes==3:
+#     signo="Áries"
+# elif mes==4:
+#     signo="Touro"
+# elif mes==5:
+#     signo="Gêmeos"
+# elif mes==6:
+#     signo="Câncer"
+# elif mes==7:
+#     signo="Leão"
+# elif mes==8:
+#     signo="Virgem"
+# elif mes==9:
+#     signo="Libra"
+# elif mes==10:
+#     signo="Escorpião"
+# elif mes==11:
+#     signo="Sagitário"
+# else:
+#     signo="Capricónio"
+
+# print(f"Seu signo é {signo}.")
+
+
+
+#---------------------------------------------------
+#visão match case
+
+# match mes:
+#     case 1:
+#         signo="Áquario"
+#     case 2:
+#         signo="Áries"
+#     case 3:
+#         signo="Touro"
+#     case 4:
+#         signo="Gêmeos"
+#     case 5:
+#         signo="Câncer"
+#     case _:
+#         signo="Número de mês inválido"
+
+# print(f"{signo}.")
+
+#--------------------------------------------------------------
+
+#Atividades
+
+POTENCIA = 3
+largura = int(input("Informe a Largura em m²:"))
+comprimento = int(input("Informe o Comprimento em m²:"))
+
+multiplicacao = largura * comprimento  #largura x comprimento
+total = (multiplicacao / POTENCIA)
+print("multiplicação é",multiplicacao,".")
+print("base para lâmpada",total,".")
+
+if total == 3:
+    lampadas= 1
+elif total >= 6:
+    lampadas= 2
+elif total >= 9:
+    lampadas= 3
+elif total >= 12:
+    lampadas= 4
+elif total >= 15:
+    lampadas= 5
+elif total >= 18:
+    lampadas= 6
+else :
+    lampadas= "Valor quebrado"
+
+print(f"A quantidade de lâmpadas necessárias para esté comodo é {lampadas}.")
+
+
+
+
+
+
---
+++ aula05/aula0501.py
@@ -0,0 +1,8 @@
+meunome = "Kathleen"
+
+for i in meunome:
+    print(i)
+
+
+for i in range(10):
+    print(i)
