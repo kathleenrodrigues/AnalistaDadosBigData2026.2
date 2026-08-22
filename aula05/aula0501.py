16f1b95bea9f64ef260c7e3cdc868aa5bbf92b6f
KATHLEEN PEREIRA RODRIGUES <kathleen.rodrigues@aluno.senac.rio>
2026-08-21 21:40:46 -0300

    atualizado 21082026

--- aula05/aula0501.py
+++ aula05/aula0501.py
@@ -1,8 +1,39 @@
-meunome = "Kathleen"
+##ESTRUTURAS DE REPETIÇÃO
 
-for i in meunome:
-    print(i)
+#FOR:
+
+# meunome = "Kathleen"
+
+# for i in meunome:
+#     print(i)
 
 
-for i in range(10):
-    print(i)
+# for i in range (10):
+#     print(i)
+
+# for i in range (1,10):
+#     print(i)
+
+# for i in range (1,10,2):
+#     print(i)
+
+# for i in range (10):
+#      print(i)
+
+# for i in range (-102,-1,2):
+#      print(i)
+
+#--------------------------------------------------
+#WHILE
+
+acertou = 0
+while acertou < 5:
+    print(f"Número {acertou + 1} de 5:") 
+    num = float(input("Digite um número: ")) 
+        
+    dobro = num * 2 
+    triplo = num * 3 
+    quádruplo = num * 4 
+        
+    print(f"  Resultado: Dobro={dobro}, Triplo={triplo}, Quádruplo={quádruplo}\n")
+    acertou+=1
